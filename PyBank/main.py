40c97b40cb3135851303abe40dc003bf192f707e
MGPena-x <mpena_14@hotmail.com>
2023-03-22 03:01:54 -0700

    made more changes, almost done with PyBank.

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,44 +10,107 @@ with open(file_path) as csvfile:
     csvheader = next(csvreader)
     #print(f'csv header: {csvheader}')
 
-# The total number of months included in the dataset
-    #csv reader file: count number of rows
-    #rows need to have contents
-    # need row count
-
     print("Financial Analysis")
     print('----------------------------------------------------------')
-
 
     total_months = 0
     total_profit_loss = 0
     
-    current = 0
-    previous = 0 
-    current_change = 0
-    previous_change = 0
+    current = 0 
+    change_a = 0
+    previous = 0
+    biggest_inc = 0
+    biggest_dec= 9999999999999999999
 
+    
     for row in csvreader:
         total_months = total_months + 1                         # The total number of months included in the dataset
         total_profit_loss = total_profit_loss + int(row[1])     # The net total amount of "Profit/Losses" over the entire period
         
-        current = int(row[1])                                    # The change in "Profit/Losses" over the entire period, and then the average of those changes
+        current = int(row[1])                                   # The change in "Profit/Losses" over the entire period, and then the average of those changes
+        
+        change_a = ((current) - (previous))
+        
         if total_months > 1:
-            current_change = (current_change + ((current) - (previous)))
-            # previous_change = (current_change + ((current) - (previous))) - (current_change - ((current) + (previous)))
-            avg_chg = current_change / (total_months - 1)          
-        previous = current
+            change_a = (change_a) + (current - previous)
+            avg_chg = (change_a) / (total_months -1) 
+            change_a = (current) - (previous)
+            previous = current
+        
+        
+        if change_a > biggest_inc:
+            biggest_inc = change_a
+        if  change_a < biggest_dec:
+            biggest_dec = change_a
 
-        # current = int(row[1])                                    # The change in "Profit/Losses" over the entire period, and then the average of those changes
-        # if total_months > 1:  
-        #     change = (change + ((current) - (previous)))       
-        # previous = current
-
+    
 
     print(f'Total Months: {total_months}')
     print(f'Total: {total_profit_loss}')
-    print(f'Average Change: {avg_chg}')
-    print(f'Previous Change: {previous_change}')
+    print(f'Average change: {avg_chg}')
+    
+    
+    print(f'Greatest Increase in Profits: {biggest_inc}')
+    print(f'Greatest Decrease in Profits: {biggest_dec}')
+    
+    # for i in range(list(change_b)):
+    #     current = range(row[1])    
+    #     change_b = (current) - (previous)        
+    #     change_b = max
+    #     change_b = min
+    # print(change_b)
+
+    # for i in range(list(change_b)):
+    # value_from_list1 = list1[change_b]
+    # value_from_list2 = list2[change_b]
+    
+    # #                                                             # print(f'Greatest Increase in Profits: {max}')
+    #   for i in range(len(list1)):
+    # value_from_list1 = list1[i]
+    # value_from_list2 = list2[i]                                                          # print(f'Greatest Decrease in Profits: {min}')
+       
+        
+        # rev_net = int(row[1])
+        # net_change = int(row[1]) - prev_net
+        # change += [net_change]
+        
+        # if prev_net > net_change:
+        #     prev_net = prev_net
+        #     prev_net = net_change
+
+        # current = int(row[1])
+        # if total_months > 1:      
+        #     stored_value = (current) - (previous)
+        #     # stored_value = max(stored_value)
+   
+
+
+        # if stored_value > current:
+        #    stored_value = stored_value
+        # else: stored_value = current   
+
+        # net_change = int(row[1]) - prev_net
+        # prev_net = int(row[1])
+        # change += [net_change]
+        
+        
+
+          # The greatest increase in profits (date and amount) over the entire period
+        # if change > current_value:
+        #     current_value = change
+        
+
+        # # The greatest decrease in profits (date and amount) over the entire period
+        # if change < current_value_2:
+        #     current_value_2 = change
+
+        # stored_value2 = change  #The greatest decrease in profits (date and amount) over the entire period
+        # if stored_value2 < current_value:
+        #     stored_value2 = stored_value2
+        # else: stored_value2 = current_value
+    
+    
+   
 
 
 
@@ -65,3 +128,8 @@ with open(file_path) as csvfile:
     # for row in csvreader:
     #     profit_loss + int(sum(row[1]))
     # print(f'Total: {profit_loss}')
+
+     # current = int(row[1])                                    # The change in "Profit/Losses" over the entire period, and then the average of those changes
+        # if total_months > 1:  
+        #     change = (change + ((current) - (previous)))       
+        # previous = current
